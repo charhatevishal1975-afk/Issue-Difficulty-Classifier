b948328f56aca4d9376723e45a65a129a67b14e5
Samarth <charhatevishal1975@gmail.com>
2026-04-02 10:25:48 +0000

    DEBUG = false

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!v@w$699+-rvsd6=b*j)eu-hfm4%&q9vd817ocutx*21+ccgt#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
